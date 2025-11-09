3bee4c5c21e051caab0cbc16e43a873740be4b8c
raquino5 <raquino5@academic.rrc.ca>
2025-11-09 02:11:15 -0600

    Fixed errors in manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,8 @@
 //= link_tree ../images
-//= link_tree ../stylesheets .css
+//= link_directory ../stylesheets .css
+//= link application.js
+//= link bootstrap.js
+//= link @popperjs--core.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
