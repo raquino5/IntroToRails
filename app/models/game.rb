301412e5b3d876d33321fb0d6767f52fdc0f3d7c
raquino5 <raquino5@academic.rrc.ca>
2025-11-09 02:20:57 -0600

    Fixed Ransack Search Setup

--- app/models/game.rb
+++ app/models/game.rb
@@ -12,4 +12,28 @@ class Game < ApplicationRecord
 
   validates :name, presence: true, length: { minimum: 2 }
   validates :slug, presence: true, uniqueness: true
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[
+      id
+      name
+      description
+      slug
+      rating
+      rawg_id
+      steam_appid
+      released
+      created_at
+      updated_at
+    ]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[
+      genres
+      developers
+      platforms
+      reviews
+    ]
+  end
 end
