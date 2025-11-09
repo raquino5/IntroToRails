40dc6c463853746feb0a31fd53f1979efdb1a782
raquino5 <raquino5@academic.rrc.ca>
2025-11-08 23:46:57 -0600

    Added simple “category” show pages for Hierarchical Collection Navigation

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,4 +1,6 @@
 class GenresController < ApplicationController
   def show
+    @genre = Genre.find(params[:id])
+    @games = @genre.games.order(rating: :desc).page(params[:page]).per(20)
   end
 end
